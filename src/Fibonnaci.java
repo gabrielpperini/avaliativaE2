9d6cf3ec6e0392928c1f388d57e532e944a04b29
camila ilges <camilailges@gmail.com>
2021-05-03 17:20:02 -0300

    codigo fibonnaci

--- src/Fibonnaci.java
+++ src/Fibonnaci.java
@@ -11,9 +11,26 @@ public class Fibonnaci {
     
     public static void main(String[] args)
     {
-        int sofs = 2;
-        int gabs = 5;
-        int mila = 8;
-        int gabs2 = 16;
+        Scanner in = new Scanner(System.in);
+        
+        int fold1 = 0;
+        int fold2 = 1;
+        int fnew = fold1 + fold2;
+
+        System.out.println("Digite um número inteiro: ");
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n - 1; i++) {
+            fold2 = fold1;
+            fold1 = fnew;
+            fnew = fold1 + fold2;
+
+            if (i == n - 1)
+            System.out.println("Na sequência de Fibonacci, o número digitado é: " +fnew);
+        }
+        
+
+    
     }
 }
+
