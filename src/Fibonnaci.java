4c689b71ddca6983a36fa7ce845d59a370b870f3
Gabriel Perini <gabrielprestesperini@hotmail.com>
2021-05-04 14:38:26 -0300

    Fibonacci: - add a error handler - fix fibonnaci method

--- src/Fibonnaci.java
+++ src/Fibonnaci.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * @author Gabriel Perini and Camila Ilges
  * @package avaliativaE2
  * 
-*/
+ */
 public class Fibonnaci {
     
     public static void main(String[] args)
@@ -15,22 +15,28 @@ public class Fibonnaci {
         
         int fold1 = 0;
         int fold2 = 1;
-        int fnew = fold1 + fold2;
+        int fnew = 0;
 
-        System.out.println("Digite um número inteiro: ");
+        System.out.print("Digite um número inteiro positivo: ");
         int n = in.nextInt();
-
-        for (int i = 1; i <= n - 1; i++) {
-            fold2 = fold1;
-            fold1 = fnew;
-            fnew = fold1 + fold2;
-
-            if (i == n - 1)
-            System.out.println("Na sequência de Fibonacci, o número digitado é: " +fnew);
+        while(n < 0){
+            System.out.println("ERROR: número não positivo.");
+            System.out.print("Digite um número inteiro positivo: ");
+            n = in.nextInt();
         }
-        
 
-    
+        if(n <= 1){
+            fnew = n;
+        }else{
+            for (int i = 1; i < n; i++) {
+                fnew = fold1 + fold2;
+                fold1 = fold2;
+                fold2 = fnew;
+            }
+        }
+
+        System.out.println("Na sequência de Fibonacci, o número digitado é: " + fnew);
+
+        in.close();
     }
 }
-
