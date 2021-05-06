7cad252874cac2eec86cc3d7bcee13b87b18ca30
camila ilges <camilailges@gmail.com>
2021-05-06 13:14:07 -0300

    change in char c

--- src/CardValidation.java
+++ src/CardValidation.java
@@ -18,11 +18,10 @@ public class CardValidation {
         int soma1 = 0;
         int soma2 = 0;
         int soma = 0;
-        int numResultantes = 0
-
-        for (int i = 8; i >= 0; i--) { // Converte os dígitos em valores inteiros, começando pelo mais a direita
-            numCartao.charAt(i);
-            char c = i;
+        int numResultantes = 0;
+        
+        for (int i = 8; i <= 0; i--) { // Converte os dígitos em valores inteiros, começando pelo mais a direita
+            char c = numCartao.charAt(i);
             int n = c - '0';
 
             if (n % 2 ==0) { // Soma os dígitos alternados
@@ -30,11 +29,13 @@ public class CardValidation {
             }
             else { // Duplica os dígitos resultantes e soma seus dígitos
                 numResultantes = n * 2;
-                soma2 = numResultantes.charAt(0) + numResultantes.charAt(1);
+                String var = numResultantes + "";
+                soma2 = var.charAt(0) + var.charAt(1);
             }
             
         soma = soma1 + soma2;
-            if (soma.charAt(1) == 0) { // Verifica se o cartão é válido
+        String var2 = soma + "";
+            if (var2.charAt(1) == 0) { // Verifica se o cartão é válido
                 System.out.println("O número do cartão é válido");
             }
             else {
